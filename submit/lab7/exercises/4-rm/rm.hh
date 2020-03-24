9a3fde73523053347cdaff6c020dc7187d18af5f
Emre Serdar <eserdar1@cs.binghamton.edu>
2020-03-23 22:57:37 -0400

    completing lab7

---
+++ submit/lab7/exercises/4-rm/rm.hh
@@ -0,0 +1,54 @@
+#ifndef OUT_HH_
+#define OUT_HH_
+
+#include <algorithm>
+#include <vector>
+
+
+template <typename Iterator>
+void
+outContainer(Iterator begin, Iterator end) 
+{
+  std::cout << "[ ";
+  auto print = [](const int& i) { std::cout << i << " " ; };
+  std::for_each(begin, end, print);
+  std::cout << "]" << std::endl;
+}
+
+/** create a vector which is identical to [begin, end) but with first
+ *  occurrence of arg removed.  Output vector elements within [ ],
+ *  separated by spaces.
+ */
+template <typename Iterator>
+void
+loopFn(Iterator begin, Iterator end, int arg)
+{
+  int isSeen=0;
+  std::vector<int> vec;
+  //@TODO
+  for (auto p=begin; p!=end; ++p){
+	if(isSeen ==0){
+		if (*p != arg){ vec.push_back(*p);}
+		else{	isSeen = 1; }
+	}
+	else{ vec.push_back(*p); }
+  } 
+	
+	 
+  outContainer(vec.begin(), vec.end());
+}
+
+/** Exactly like loopFn() but no loops should be used */
+template <typename Iterator>
+void noLoopFn(Iterator begin, Iterator end, int arg)
+{
+  //@TODO
+  auto x=std::find(begin, end, arg);
+  std::vector<int> vec(begin, x);
+  if (x != end) {
+		std::copy(++x, end, std::back_inserter(vec));
+  }
+  outContainer(vec.begin(), vec.end());
+}
+
+#endif //ifndef OUT_HH_
--- submit/prj2-sol/nums.cc
+++ submit/prj2-sol/nums.cc
@@ -4,8 +4,10 @@
 
 using TestType = int;
 void output_from_seq (ConstIter<TestType> *begin, ConstIter<TestType> *end){
-	for (;begin, end; ++begin, --end) {
-		std::cout << *begin << ' ' << *end ;	
+	ConstIter<TestType> *it1= begin;
+	ConstIter<TestType> *it2= end;
+	for (ConstIter<TestType>&p = *it1, &p2 = *it2; p,p2; ++p, --p2  ) {
+		std::cout << " " << *p << " " << *p2;	
 	} 
  	std::cout << std::endl;
 }
@@ -27,21 +29,25 @@ void readInt(std::string filename, Seq<TestType> *x){
 
 
 void verify_args(int argc, char * argv[]){
-	if (argc > 3 || argc<2 ){
-	
+	if (argc > 3 || argc<2 ){ 
+		std::cerr << "usage: ./nums [-a] INTS_FILE_PATH " << std::endl;
+		exit(1);		
 	}
-	else if ( argc == 2){
-		if (argv[1] == "-a"){
-		
+	else if ( argc == 3){
+		if (std::string(argv[1]) == "-a"){
+			SeqPtr<TestType> obj1 = ArraySeq<TestType>::make();
+			readInt(std::string(argv[2]), obj1.get()); // "-a" is the second argument so wee need to take third argument with argv[2]
 		}
 		else{
+			std::cerr << "usage: ./nums [-a] INTS_FILE_PATH " << std::endl;
+			exit(1);
+		
 		}
 	}	
 }
 
 int main(int argc, char *argv[]){
-	SeqPtr<TestType> obj1 = ArraySeq<TestType>::make();
-	readInt(std::string(argv[1]), obj1.get());
+	verify_args(argc, argv);
 	return 0;
 	
 }
